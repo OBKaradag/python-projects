30d29c7706a9110870866925b3d15e4146652b4e
Ömer Burak Karadağ <omerkaradag5509@gmail.com>
2026-09-10 22:42:58 +0300

    V1.1

---
+++ kullanici_adi_sifre.py
@@ -0,0 +1,8 @@
+kullanici_adi="ÖBkrdg"
+sifre="ömer1234"
+klncadi=str(input("kullanıcı adı:"))
+sfr=input("şifre:")
+if kullanici_adi is klncadi and sifre==sfr:
+        print("Hoşgeldiniz...")
+else:
+    print("Hatalı giriş...")
